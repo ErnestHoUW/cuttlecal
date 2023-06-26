9b9a7228ae02b59ad0267d849a331ae9ce3a1a2a
Ernest Ho <ernest.ho.001@gmail.com>
2023-06-26 13:41:25 -0400

    add exposure testing

---
+++ Code Snippets/exposure-testing.py
@@ -0,0 +1,18 @@
+import cv2
+
+stream = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+print(stream.get(cv2.CAP_PROP_EXPOSURE))
+stream.set(cv2.CAP_PROP_EXPOSURE, 0) 
+
+start_interval=0
+sum_latency=0
+amount_of_frames=1000
+for i in range(amount_of_frames):
+    _, frame = stream.read()
+
+    cv2.imshow("Webcam_Output", frame)
+    cv2.waitKey(1)
+
+
+stream.release()
+cv2.destroyAllWindows
