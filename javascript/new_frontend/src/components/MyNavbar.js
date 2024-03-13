d60ac69a170b22b92bbbef8add4e77a16a7a3bc1
Ernest Ho <ernest.ho.001@gmail.com>
2024-03-12 23:15:04 -0400

    Fix Navbar

--- javascript/new_frontend/src/components/MyNavbar.js
+++ javascript/new_frontend/src/components/MyNavbar.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
 import { Link } from "react-router-dom";
-import CuttleCal from "../images/cuttlecal_white.png"
-import "../styles/MyNavbar.css";
+import CuttleCal from "../images/cuttlecal_white.png";
 
 export default function MyNavbar() {
+
+  const linkStyle = {
+    color: 'inherit',
+    textDecoration: 'none',
+    display: 'flex',
+    alignItems: 'center',
+  };
+
+  const imageStyle = {
+    height: "30px",
+    paddingRight: "10px",
+  };
+
   return (
     <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="/" style={{ marginLeft: '20px', display: "flex", gap: '10px' }}>
-          <img src={CuttleCal} style={{ "height":"30px" }} alt=""/>
+      <Navbar.Brand style={{ marginLeft: '20px', display: "flex", alignItems: 'center' }}>
+        <Link to="/" style={linkStyle}> {}
+          <img src={CuttleCal} style={imageStyle} alt=""/> {}
           CuttleCal
+        </Link>
       </Navbar.Brand>
-      <Nav>
-          <Nav.Link as={Link} to="/calibration">Calibration</Nav.Link>
-          <Nav.Link as={Link} to="/upload">Upload</Nav.Link>
-          <Nav.Link as={Link} to="/compare">Colour Compare</Nav.Link>
-          <Nav.Link as={Link} to="/image">Image Compare</Nav.Link>
+      <Nav className="align-items-center"> {/* Ensures that navigation links are also vertically centered */}
+        <Nav.Link as={Link} to="/calibration" >Calibration</Nav.Link>
+        <Nav.Link as={Link} to="/upload" >Upload</Nav.Link>
+        <Nav.Link as={Link} to="/compare" >Colour Compare</Nav.Link>
+        <Nav.Link as={Link} to="/image" >Image Compare</Nav.Link>
       </Nav>
     </Navbar>
   )
